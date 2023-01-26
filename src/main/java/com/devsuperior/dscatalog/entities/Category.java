56550ac32d6f08c054f05c71a0499cdfddd0f7d9
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-26 15:59:36 -0300

    Creating CategoryService and CategoryRepository

--- src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,16 +1,36 @@
 package com.devsuperior.dscatalog.entities;
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
+@Entity // This annotation is used to indicate that this class is an entity
+@Table(name = "tb_category") // This annotation is used to indicate the name of the table in the database
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id  // This annotation is used to indicate that this attribute is the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // This annotation is used to indicate that the primary key is auto-increment
     @Getter @Setter private Long id;
     @Getter @Setter private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
--- src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,25 +1,26 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController // This annotation is used to indicate that this class is a REST controller
 @RequestMapping(value = "/categories") // This annotation is used to indicate the route of the resource
 public class CategoryResource {
-    // creating the first end point
 
+    @Autowired
+    private CategoryService categoryService;
+
+    // creating the first end point
     @GetMapping // This annotation is used to indicate that this method is a GET method
     public ResponseEntity<List<Category>> findAll(){  // ResponseEntity is a type that allows to return responses from web services
-        List<Category> list = new ArrayList<>(); // creating a list of categories
-
-        list.add(new Category(1L, "Books")); // adding categories to the list
-        list.add(new Category(2L, "Electronics")); // adding categories to the list
+        List<Category> list = categoryService.findAll(); // calling the findAll method from the service
 
         return ResponseEntity.ok().body(list); // returning the list of categories
     }
