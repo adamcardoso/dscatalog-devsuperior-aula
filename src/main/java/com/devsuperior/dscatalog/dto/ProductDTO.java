41cb78d9e35ad86c8ef3d905fef1806ce2c4ae8f
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-28 02:50:48 -0300

    ProductDTO

---
+++ src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -0,0 +1,62 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+@NoArgsConstructor
+public class ProductDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+    @Getter @Setter private String name;
+
+    @Column(columnDefinition = "TEXT")
+    @Getter @Setter private String description;
+    @Getter @Setter private Double price;
+    @Getter @Setter private String imgUrl;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Getter @Setter private Instant date;
+
+    @Getter @Setter private List<CategoryDTO> categoriesDTO = new ArrayList<>();
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+        this.date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categoriesDTO) { // Constructor with categories
+        this(entity);
+        categoriesDTO.forEach(cat -> this.categoriesDTO.add(new CategoryDTO(cat)));  // Add all categories to the list
+    }
+}
