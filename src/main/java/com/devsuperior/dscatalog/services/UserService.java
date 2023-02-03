86ee7197ceb570ad2edcd21d988d0f2a2876268f
adamcardoso <adamjuniorcardoso@gmail.com>
2023-02-03 15:42:09 -0300

    AuthorizationServerConfig

--- src/main/java/com/devsuperior/dscatalog/services/UserService.java
+++ src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.services;
 
 
 
+import java.util.Objects;
 import java.util.Optional;
 
 import com.devsuperior.dscatalog.dto.RoleDTO;
@@ -11,11 +12,16 @@ import com.devsuperior.dscatalog.repositories.RoleRepository;
 import com.devsuperior.dscatalog.services.expections.DatabaseException;
 import com.devsuperior.dscatalog.services.expections.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +31,9 @@ import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 
 @Service //Service is a component that is used to implement business rules
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -102,4 +110,15 @@ public class UserService {
         }
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (Objects.isNull(user)){
+            logger.error("User not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+        logger.info("User found: " + username);
+
+        return user;
+    }
 }
